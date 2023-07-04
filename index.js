b2b0413fedfde27483f34f7bde9086ecf69c56fb
hero <wmc15156@naver.com>
2023-07-04 18:34:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//TODO: 내일은 하자
+console.log('여러줄에 걸쳐\n 표시됩니다.')
+console.log('문자열이 긴 경우에는' + ' ' + '문자열을' + '나눈 뒤' + '다시 합칩니다.')
+
+
+let total = 20;
+total = 20 + 10;
+console.log(total);
+
+console.clear();
+const data = 'data';
+const data = 'data2';
